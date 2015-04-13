22c2fab0be0f22a90aaef37a3f1b987b8adcd206
Pascal de Vink <pascal.de.vink@gmail.com>
2015-04-13 20:27:45 +0200

    Automated some standard slide data

--- js/theme/amsterdamphp.js
+++ js/theme/amsterdamphp.js
@@ -1,3 +1,15 @@
+function fillSlideData()
+{
+    for(var index in slideData["text"]) {
+        $("#"+index).html(slideData["text"][index]);
+    }
+
+    for(var index in slideData["images"]) {
+        $("#"+index).attr("src", slideData["images"][index]["src"]);
+        $("#"+index).attr("alt", slideData["images"][index]["alt"]);
+    }
+}
+
 function enhanceSpeakerImage() {
   var speakerInfo = $("img.speaker").parent();
   $(speakerInfo).corner("bite 20px");
